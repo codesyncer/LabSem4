f3d177e13354bcabde3cc09326fb5fc90192dad7
srinskit <srinagrao2007@gmail.com>
2018-02-01 23:37:25 +0530

    Completed 3 circles

--- CG/LabX/main.cpp
+++ CG/LabX/main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 const int width = 500, height = 500;
 
-void drawLine(int x1, int y1, int x2, int y2) {
+void drawLineDDA(int x1, int y1, int x2, int y2) {
     int dx = x2 - x1, dy = y2 - y1;
     int steps = max(abs(dx), abs(dy));
     float xInc = (float) dx / steps, yInc = (float) dy / steps;
@@ -22,10 +22,70 @@ void drawLine(int x1, int y1, int x2, int y2) {
     glEnd();
 }
 
+void drawLineMidPoint(int x1, int y1, int x2, int y2) {}
+
+void drawCircleNaive(int xc, int yc, int r) {
+    int y;
+    glBegin(GL_POINTS);
+    for (int x = 0; x <= r; ++x) {
+        y = (int) round(sqrt(r * r - x * x));
+        glVertex2i(xc + x, yc + y);
+        glVertex2i(xc - x, yc + y);
+        glVertex2i(xc + x, yc - y);
+        glVertex2i(xc - x, yc - y);
+    }
+    glEnd();
+}
+
+void drawCircleAngular(int xc, int yc, int r, float step = -1) {
+    int x, y;
+    float o = 0;
+    if (step == -1) step = 1.0f / r;
+    glBegin(GL_POINTS);
+    while (o <= M_PI / 4) {
+        x = (int) round(r * cos(o)), y = (int) round(r * sin(o));
+        glVertex2i(xc + x, yc + y);
+        glVertex2i(xc - x, yc + y);
+        glVertex2i(xc + x, yc - y);
+        glVertex2i(xc - x, yc - y);
+        glVertex2i(xc + y, yc + x);
+        glVertex2i(xc - y, yc + x);
+        glVertex2i(xc + y, yc - x);
+        glVertex2i(xc - y, yc - x);
+        o += step;
+    }
+    glEnd();
+}
+
+void drawCircleMidPoint(int xc, int yc, int r) {
+    int x = 0, y = r, p = 5 / 4 - r;
+    glBegin(GL_POINTS);
+    while (x <= y) {
+        glVertex2i(xc + x, yc + y);
+        glVertex2i(xc - x, yc + y);
+        glVertex2i(xc + x, yc - y);
+        glVertex2i(xc - x, yc - y);
+        glVertex2i(xc + y, yc + x);
+        glVertex2i(xc - y, yc + x);
+        glVertex2i(xc + y, yc - x);
+        glVertex2i(xc - y, yc - x);
+        if (p >= 0) {
+            --y;
+            p -= 2 * y;
+        }
+        ++x;
+        p += 1 + 2 * x;
+    }
+    glEnd();
+}
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-    drawLine(100, 100, 400, 150);
+    drawLineDDA(-100, -100, 200, 150);
+    drawLineMidPoint(100, 100, -200, -150);
+    drawCircleNaive(0, 0, 225);
+    drawCircleAngular(0, 0, 200);
+    drawCircleMidPoint(45, 45, 200);
     glutSwapBuffers();
 }
 
@@ -34,15 +94,16 @@ void reshape(int w, int h) {
 }
 
 void initOpenGL() {
-    glOrtho(0, 500, 0, 500, 0, 500);
+    glOrtho(-250, 250, -250, 250, -250, 250);
     glClearColor(0, 0, 0, 0);
+    glColor3f(0, 1, 0);
 }
 
 int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
     glutInitWindowSize(width, height);
-    glutInitWindowPosition(2200, 200);
+    glutInitWindowPosition(1200, 100);
     glutCreateWindow("CG");
     initOpenGL();
     glutDisplayFunc(display);
