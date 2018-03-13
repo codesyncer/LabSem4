71e5ba7634173619b29f8568f8df746ee8585f6b
srinskit <srinagrao2007@gmail.com>
2018-03-13 08:39:47 +0530

    CCN lab6

---
+++ CCN/Lab6/Server/main.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <unistd.h>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define SERVER_PORT 8091
+
+int main(int argc, char **argv) {
+    char s[80], f[80];
+    FILE *fp;
+    sockaddr_in serverAddress = sockaddr_in(), clientAddress = sockaddr_in();
+    int listenFd, connFd, clientLen;
+    bzero(&serverAddress, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(SERVER_PORT);
+    listenFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenFd == -1) {
+        perror("Could not create socket");
+        return 0;
+    }
+    if (bind(listenFd, (sockaddr *) &serverAddress, sizeof(serverAddress)) == -1) {
+        perror("Could not bind socket");
+        close(listenFd);
+        return 0;
+    }
+    listen(listenFd, 2);
+    clientLen = sizeof(clientAddress);
+    connFd = accept(listenFd, (sockaddr *) &clientAddress, (socklen_t *) &clientLen);
+    if (connFd == -1) {
+        perror("Could not accept socket");
+        close(listenFd);
+        return 0;
+    }
+    printf("Client Connected!\n");
+    read(connFd, f, 80);
+    fp = fopen(f, "r");
+    printf("Filename: %s\n", f);
+    if (fp == nullptr) {
+        strcpy(s, "FileNotFound");
+        write(connFd, s, sizeof(s));
+    } else {
+        while (fgets(s, 80, fp) != nullptr) {
+            printf("%s", s);
+            write(connFd, s, sizeof(s));
+        }
+        fclose(fp);
+    }
+    close(listenFd);
+    return 0;
+}
